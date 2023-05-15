3f4758cf28955fec36f2da9e8c1957fb9017bb7d
João Thiago Nunes <95290892+JoaoThiagoNunes@users.noreply.github.com>
2023-05-15 10:53:48 -0300

    Delete Django Form directory

--- Django Form/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ContatoForm(forms.Form):
-    nome = forms.CharField(label='Nome', max_length=100, min_length=3)
-    email = forms.EmailField(label='Email', max_length=100)
-    assunto = forms.CharField(label='Assunto', max_length=120)
-    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
-    
--- Django Form/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import contato,index
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('contato/', contato, name='contato'),
-
-]
